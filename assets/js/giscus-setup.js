2a5a31b91e05818ab18a756fcf1a1c3996a4ccc7
BruDiGe <97832125+BruDiGe@users.noreply.github.com>
2025-12-07 21:19:05 +0000

    Deploying to gh-pages from @ BruDiGe/alfolio@5b26d5ea3f1d4ddd4ecc34312ab13aa5a74d330e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "BruDiGe/tutorial",
+    "data-repo": "BruDiGe/alfolio",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
